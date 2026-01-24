e82b8d9dfc10026ee5a4084f41f028b0cce5ece4
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2026-01-24 12:30:49 +0530

    Update max_consecutive_ones.py

--- Arrays/Easy/max_consecutive_ones.py
+++ Arrays/Easy/max_consecutive_ones.py
@@ -10,3 +10,16 @@ class Solution:
                 cnt=0
         return maxl
         
+# Using Two pointer approach
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        left = 0
+        max_len = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                left = right + 1   # move window start
+            else:
+                max_len = max(max_len, right - left + 1)
+
+        return max_len
