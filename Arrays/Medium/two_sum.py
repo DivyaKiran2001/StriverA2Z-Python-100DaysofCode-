4eeeeb4e8e85075592c667f5a5574d451000d944
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2026-01-27 17:21:21 +0530

    Update two_sum.py

--- Arrays/Medium/two_sum.py
+++ Arrays/Medium/two_sum.py
@@ -23,4 +23,24 @@ class Solution:
                 return [hm[rem],i]
             hm[nums[i]]=i
         return [-1,-1]
+        
+# Optimal - Using Two pointer Approach
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_with_indices=[(num,idx) for idx,num in enumerate(nums)]
+        nums_with_indices.sort(key=lambda x:x[0])
+        left=0
+        right=len(nums)-1
+        while left<right:
+            current_sum=nums_with_indices[left][0]+nums_with_indices[right][0]
+            if current_sum==target:
+                return [nums_with_indices[left][1], nums_with_indices[right][1]]
+            elif current_sum<target:
+                left+=1
+            else:
+                right-=1
+        return [-1,-1]
+
+
 
