968fc42a4586a0bc9080633a5e37f864d143d0f6
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2026-02-09 14:51:31 +0530

    Create minimum_window_substring.py

---
+++ Arrays/Sliding_Window_With_Two_Pointers/minimum_window_substring.py
@@ -0,0 +1,40 @@
+# Brute
+
+## Generate all substrings and find the occurences of t in string s using hashmap
+
+# Optimal (Using Sliding Window with two pointers)
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        minlen=float('inf')
+        start_idx=0
+        hash_arr=[0]*128
+        cnt=0
+        l=0
+        r=0
+        for ch in t:
+            hash_arr[ord(ch)]+=1
+        
+        while r<len(s):
+            ch=s[r]
+            if hash_arr[ord(ch)]>0:
+                cnt+=1
+            hash_arr[ord(ch)]-=1
+            while cnt==len(t):
+                if r-l+1<minlen:
+                    minlen=r-l+1
+                    start_idx=l
+                hash_arr[ord(s[l])]+=1
+                if hash_arr[ord(s[l])]>0:
+                    cnt-=1
+                l+=1
+            r+=1
+
+            # Step 4: Return result
+        if minlen == float('inf'):
+            return ""
+        return s[start_idx:start_idx +minlen]
+        
+        
+
+
+        
