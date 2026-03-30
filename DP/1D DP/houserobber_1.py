dff4a18298163995fa531bac49dd1997c9a1beb8
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2026-03-30 18:27:54 +0530

    Update houserobber_1.py

--- DP/1D DP/houserobber_1.py
+++ DP/1D DP/houserobber_1.py
@@ -1,3 +1,4 @@
+# Memoization
 class Solution:
     def solve(self,arr,i,dp):
         if i<0:
@@ -17,4 +18,31 @@ class Solution:
         dp=[-1]*n
         return self.solve(nums,n-1,dp)
 
+# Tabulation
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        dp[0]=nums[0]
+        for i in range(1,n):
+            pick=nums[i]
+            if i>1:
+                pick+=dp[i-2]
+            notpick=0+dp[i-1]
+            dp[i]=max(pick,notpick)
+        return dp[n-1]
+# Space Optimization
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev2=0
+        prev=nums[0]
+        for i in range(1,len(nums)):
+            pick=nums[i]+prev2
+            notpick=prev
+            curr=max(pick,notpick)
+            prev2=prev
+            prev=curr
+        return prev
+        
         
